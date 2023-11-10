9832cbce4c018034942085a342b35c67dfa36994
bhavithaJ <149118649+bhavithaJ@users.noreply.github.com>
2023-11-09 22:37:24 -0600

    Add files via upload

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,1 +1,24 @@
+import Doctor from "./Doctor.js"
+import Patient from "./Patient.js"
+import Insurer from "./Insurer.js"
+import { useEffect, useState } from "react"
 
+const Dashboard = ({SmartH, token, account, ipfs}) => {
+    const [ethValue, setEthValue] = useState(0);
+
+    useEffect(() => {
+        fetch('https://api.coinbase.com/v2/exchange-rates?currency=ETH')
+            .then(res => res.json())
+            .then(res => setEthValue(res.data.rates.INR))
+        if(token==="") window.location.href = '/login'
+    }, [])
+
+    return (
+        <div className="dash">
+            {token==="1" ? <Patient ipfs={ipfs} ethValue={ethValue} SmartH={SmartH} account={account} /> : token==="2" ? <Doctor ipfs={ipfs} SmartH={SmartH} account={account} /> : token==="3" ? <Insurer ipfs={ipfs} ethValue={ethValue} SmartH={SmartH} account={account} /> : <></>}
+        </div>
+    )
+}
+
+
+export default Dashboard
